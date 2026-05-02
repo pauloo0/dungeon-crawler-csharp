aec33f3ac9f6b0570ef0ffb37787119013809a17
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2026-05-02 01:34:28 +0100

    create character class and use it to create the player and enemy entities

---
+++ Character.cs
@@ -0,0 +1,19 @@
+public class Character
+{
+  public string Name { get; set; }
+  public int MaxHP { get; set; }
+  public int CurrentHP { get; set; }
+  public int Attack { get; set; }
+  public int Defence { get; set; }
+
+  public Character(string name, int maxHP = 100, int attack = 15, int defence = 5)
+  {
+    Name = name;
+    MaxHP = maxHP;
+    CurrentHP = maxHP;
+    Attack = attack;
+    Defence = defence;
+  }
+
+  public bool IsAlive => CurrentHP > 0;
+}
--- Program.cs
+++ Program.cs
@@ -10,29 +10,45 @@ string? startGame = Console.ReadLine();
 if (startGame == "" || startGame is null) startGame = "y";
 if (startGame == "n") return;
 
+Character player = new(heroName);
+Character enemy = new("Gurk", 100, 10, 2);
 Console.Clear();
-string? move = "";
-bool validMove = false;
-Console.WriteLine("Oh no! An enemy has appeared from the shadows. It's a Goblin!");
 
-while (move == "" || !validMove)
+Console.WriteLine("Oh no! An enemy has appeared from the shadows. It's a Goblin named {0}!", enemy.Name);
+while (player.IsAlive && enemy.IsAlive)
 {
-  Console.WriteLine("What do you wish to do?");
-  Console.WriteLine("1: Attack   2: Defend");
-  move = Console.ReadLine();
-  validMove = new[] { "1", "2" }.Contains(move);
-
-  if (!validMove)
+  string? move = "";
+  bool validMove = false;
+  while (move == "" || !validMove)
   {
-    Console.WriteLine("Invalid move. Try again!");
+    Console.WriteLine("Your HP: {0}       {1}'s HP: {2}", player.CurrentHP, enemy.Name, enemy.CurrentHP);
+    Console.WriteLine("What do you wish to do?");
+    Console.WriteLine("1: Attack   2: Defend");
+    move = Console.ReadLine();
+    validMove = new[] { "1", "2" }.Contains(move);
+
+    if (!validMove)
+    {
+      Console.WriteLine("Invalid move. Try again!");
+    }
+  }
+
+  if (move == "1")
+  {
+    Console.WriteLine("You attacked and dealt {0} damage.", "some");
+  }
+  if (move == "2")
+  {
+    Console.WriteLine("You defended and mitigated {0} damage. You lost {1} HP.", "some", "some");
   }
 }
 
-if (move == "1")
+if (!player.IsAlive)
 {
-  Console.WriteLine("You attacked and dealt {0} damage.", "some");
+  Console.WriteLine("You died! Better luck next time.");
 }
-if (move == "2")
+
+if (!enemy.IsAlive)
 {
-  Console.WriteLine("You defended and mitigated {0} damage. You lost {1} HP.", "some", "some");
+  Console.WriteLine("You defeated {0}. Congratiolations!", enemy.Name);
 }
